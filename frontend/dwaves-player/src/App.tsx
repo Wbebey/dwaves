b29d80adc0b7f13bc4605532d57e1a63cbed7da5
Ubuntu <ubuntu@vps-7d26a412.vps.ovh.ca>
2022-07-29 11:16:31 +0000

    feature[#2pcbfvu]: new player style

---
+++ frontend/dwaves-player/src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Player } from './Pages/Player';
+import { Loader } from './Components/Loader';
+import { Divider } from '@mantine/core';
+
+
+function App() {
+
+  const [loader , setLoader] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false)
+    }, 3000)
+  })
+
+  return loader ? ( <Loader/> ) : (
+    <div className="App">
+      <Router>
+          {/* A <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Routes>
+            <Route path="/" element={<Player/>} />
+            {/* <Route path="/loading" element={<Loader/>} /> */}
+          </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
