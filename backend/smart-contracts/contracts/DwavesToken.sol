adf1075f4e69b12dee78417e1f291cddae832609
Alex <alexandre@yakow.io>
2022-10-01 19:34:15 +0200

    feat: add deploy/interact scripts + basic token tests

--- backend/smart-contracts/contracts/DwavesToken.sol
+++ backend/smart-contracts/contracts/DwavesToken.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.17;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract VibesToken is ERC20 {
+contract DwavesToken is ERC20 {
     string constant NAME = "Dwaves";
     string constant SYMBOL = "VIBES";
     uint8 constant DECIMALS = 18;
-    uint256 constant INITIAL_SUPPLY = 300_000_000_000 * 10**DECIMALS;
+    uint256 constant INITIAL_SUPPLY = 300_000_000 * 10**DECIMALS;
 
     constructor() ERC20(NAME, SYMBOL) {
         _mint(msg.sender, INITIAL_SUPPLY);
