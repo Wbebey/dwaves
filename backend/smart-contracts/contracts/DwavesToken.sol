e8a1140c4483ba70285b89c7fb1c576c0ea34462
Alex <alexandre@yakow.io>
2022-10-30 22:59:09 +0100

    feat: ArtistPayer contract

--- backend/smart-contracts/contracts/DwavesToken.sol
+++ backend/smart-contracts/contracts/DwavesToken.sol
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract DwavesToken is ERC20, ERC20Burnable {
+contract DwavesToken is ERC20, ERC20Burnable, AccessControl {
     string constant NAME = "Dwaves";
     string constant SYMBOL = "VIBES";
     uint8 constant DECIMALS = 18;
     uint256 constant INITIAL_SUPPLY = 300_000_000 * 10**DECIMALS;
+    bytes32 constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20(NAME, SYMBOL) {
+    constructor(address minter) ERC20(NAME, SYMBOL) {
         _mint(msg.sender, INITIAL_SUPPLY);
+        _setupRole(MINTER_ROLE, minter);
+    }
+
+    function batchMint(address[] calldata to_list, uint256[] calldata amounts)
+        external
+    {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        require(to_list.length == amounts.length);
+
+        for (uint256 i = 0; i < to_list.length; i++) {
+            _mint(to_list[i], amounts[i]);
+        }
     }
 }
