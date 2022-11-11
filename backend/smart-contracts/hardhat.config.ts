c185b6d149d8715ab22831907a29daadf0f48206
Alex <alexandre@yakow.io>
2022-11-11 14:01:37 +0100

    chore: cleanup & update .env examples

--- backend/smart-contracts/hardhat.config.ts
+++ backend/smart-contracts/hardhat.config.ts
@@ -4,10 +4,9 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const { ALCHEMY_API_KEY, DWAVES_DEPLOYER_PRIVATE_KEY } = process.env
 
-if (!GOERLI_PRIVATE_KEY || !ALCHEMY_API_KEY) {
+if (!DWAVES_DEPLOYER_PRIVATE_KEY || !ALCHEMY_API_KEY) {
   console.error('💥 error loading env')
   process.exit(1)
 }
@@ -18,7 +17,7 @@ const config: HardhatUserConfig = {
   networks: {
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY],
+      accounts: [DWAVES_DEPLOYER_PRIVATE_KEY],
     },
   },
 }
