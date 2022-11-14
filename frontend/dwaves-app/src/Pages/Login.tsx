1e742c7aa93c0b945c8162aba17857c57d6bd91b
BryanSeychelles <bryanseychellesyk@gmail.com>
2022-11-14 17:00:10 +0000

    feat: Set up archi in App.tsx to welcome the modal to register or login a account web2

---
+++ frontend/dwaves-app/src/Pages/Login.tsx
@@ -0,0 +1,16 @@
+import "../Styles/Login.scss"
+
+export const ModalLogin = () => {
+    return (
+        <div id='contain-modal'>
+            <div id="modal">
+                <header>
+                    
+                    <h1>
+                        Login
+                    </h1>
+                </header>
+            </div>
+        </div>
+    )
+}
