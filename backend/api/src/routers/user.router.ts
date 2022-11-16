6c5b196e6fb32bc6f162ee8bd597969824137fc8
Alex <alexandre@yakow.io>
2022-11-16 23:24:25 +0100

    feat: api config/controller/router setup

---
+++ backend/api/src/routers/user.router.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+import UserController from '@controllers/user.controller'
+
+const userRouter = Router()
+const userController = new UserController()
+
+userRouter.get('/', userController.get)
+
+export default userRouter
