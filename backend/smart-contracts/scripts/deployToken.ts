07af10f579b6fe2cd9e1d279e9f9b4bbde502932
Alex <alexandre@yakow.io>
2022-11-16 19:06:36 +0100

    feat: smart contract deploy script

--- backend/smart-contracts/scripts/deployToken.ts
+++ backend/smart-contracts/scripts/deployToken.ts
@@ -1,63 +1,41 @@
 import { ethers } from 'hardhat'
-import { promises as fs } from 'fs'
-import { Contract } from 'ethers'
+
+import { deploy, writeABI, cleanup, handleError } from '../utils/deployHelpers'
 import { ArtistPayer, DwavesToken } from '../typechain-types'
 
 const main = async () => {
-  const [contractOwner] = await ethers.getSigners()
-  console.log(`Deploying from ${contractOwner.address}`)
+  const [deployer, payer, bank] = await ethers.getSigners()
+
+  console.log(`👷 Deployer address: ${deployer.address}`)
 
   const tokenName = 'DwavesToken'
-  const token = (await deploy(tokenName)) as DwavesToken
+  const dwavesToken = (await deploy(
+    tokenName,
+    deployer,
+    bank.address
+  )) as DwavesToken
   const payerName = 'ArtistPayer'
-  const payer = (await deploy(payerName, token.address)) as ArtistPayer
+  const artistPayer = (await deploy(
+    payerName,
+    deployer,
+    dwavesToken.address
+  )) as ArtistPayer
 
   const [MINTER_ROLE, PAYER_ROLE] = await Promise.all([
-    token.MINTER_ROLE(),
-    payer.PAYER_ROLE(),
+    dwavesToken.MINTER_ROLE(),
+    artistPayer.PAYER_ROLE(),
   ])
   await Promise.all([
-    token.grantRole(MINTER_ROLE, payer.address),
-    payer.grantRole(PAYER_ROLE, contractOwner.address),
+    dwavesToken.grantRole(MINTER_ROLE, artistPayer.address),
+    artistPayer.grantRole(PAYER_ROLE, payer.address),
   ])
 
-  console.log('Generating abis...')
-  await Promise.all([writeABI(token, tokenName), writeABI(payer, payerName)])
+  await Promise.all([
+    writeABI(dwavesToken, tokenName),
+    writeABI(artistPayer, payerName),
+  ])
 
-  console.log('DONE')
-}
-
-const deploy = async (
-  contractName: string,
-  ...args: any[]
-): Promise<Contract> => {
-  const factory = await ethers.getContractFactory(contractName)
-  const contract = await factory.deploy(...args)
-  await contract.deployed()
-
-  console.log(
-    `${contractName} contract successfully deployed to: ${contract.address}`
-  )
-
-  return contract
-}
-
-const writeABI = async (
-  contract: Contract,
-  filename: string
-): Promise<void> => {
-  const data = {
-    address: contract.address,
-    abi: JSON.parse(contract.interface.format('json') as string),
-  }
-  await fs.writeFile(`../abi/${filename}.json`, JSON.stringify(data))
-}
-
-const cleanup = () => process.exit(0)
-
-const handleError = (e: Error) => {
-  console.error(e)
-  process.exitCode = 1
+  console.log('😎 DONE')
 }
 
 main().then(cleanup).catch(handleError)
