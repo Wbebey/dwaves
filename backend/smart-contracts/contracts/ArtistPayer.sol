23922903eac385863ce23a1edba8a974ae7479a0
Alex <alexandre@yakow.io>
2022-11-16 18:57:06 +0100

    feat: add bank/payer accounts & refactor ArtistPayer/DwavesToken contracts

--- backend/smart-contracts/contracts/ArtistPayer.sol
+++ backend/smart-contracts/contracts/ArtistPayer.sol
@@ -5,28 +5,66 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import {DwavesToken} from "./DwavesToken.sol";
 
 contract ArtistPayer is AccessControl {
-    uint256 public tokens_per_listening;
     DwavesToken token;
     bytes32 public constant PAYER_ROLE = keccak256("PAYER_ROLE");
 
+    event TokenPayments(address[] addresses, uint256[] amounts);
+
     constructor(DwavesToken _token) {
         token = _token;
-        tokens_per_listening = (6 * 10**token.decimals()) / 10**2;
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     function payArtists(
-        address[] calldata artistAddresses,
-        uint256[] calldata listenings
+        address[] calldata _artistAddresses,
+        uint256[] calldata _listenings
     ) external {
-        require(hasRole(PAYER_ROLE, msg.sender), "Caller is not a payer");
-        require(artistAddresses.length == listenings.length);
+        _preValidatePayments(_artistAddresses, _listenings);
 
-        uint256[] memory amounts = new uint256[](listenings.length);
-        for (uint256 i = 0; i < artistAddresses.length; i++) {
-            amounts[i] = listenings[i] * tokens_per_listening;
+        uint256[] memory amounts = _calculateAmounts(
+            _artistAddresses,
+            _listenings
+        );
+
+        _processPayments(_artistAddresses, amounts);
+        emit TokenPayments(_artistAddresses, amounts);
+    }
+
+    function _preValidatePayments(
+        address[] calldata _artistAddresses,
+        uint256[] calldata _listenings
+    ) internal view onlyRole(PAYER_ROLE) {
+        require(
+            _artistAddresses.length == _listenings.length,
+            "ArtistPayer: addresses and listenings do not have same length"
+        );
+    }
+
+    function _calculateAmounts(
+        address[] calldata _artistAddresses,
+        uint256[] calldata _listenings
+    ) internal view returns (uint256[] memory) {
+        uint256[] memory amounts = new uint256[](_listenings.length);
+        for (uint256 i = 0; i < _artistAddresses.length; i++) {
+            amounts[i] = _getTokenAmount(_listenings[i]);
         }
 
-        token.batchMint(artistAddresses, amounts);
+        return amounts;
+    }
+
+    function _getTokenAmount(uint256 _listenings)
+        internal
+        view
+        returns (uint256)
+    {
+        // 0.06 * token decimals
+        return _listenings * 6 * (10**token.decimals() / 10**2);
+    }
+
+    function _processPayments(
+        address[] calldata _artistAddresses,
+        uint256[] memory _amounts
+    ) internal {
+        token.batchMint(_artistAddresses, _amounts);
     }
 }
