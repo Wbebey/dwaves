d6873952a0acc0c0233ca46dbf92745a75a5cadf
Bastien FROMENT <bastien@oplit.fr>
2022-11-24 16:05:42 +0100

    fix: enable absolute imports and clean unused prop

---
+++ frontend/dwaves-app/src/Components/shared/index.ts
@@ -0,0 +1,9 @@
+import { Button } from "components/shared/Button";
+import { Field } from "components/shared/Field";
+import { Icon } from "components/shared/Icon";
+
+export {
+  Button,
+  Field,
+  Icon,
+};
