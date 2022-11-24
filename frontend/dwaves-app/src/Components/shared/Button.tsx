1b9e27d297960b27e24e5ef6f915e49e70fbb714
Bastien FROMENT <bastien@oplit.fr>
2022-11-24 16:59:40 +0100

    fix: add aliases for style imports and fix missing styles, add early shared Button component

--- frontend/dwaves-app/src/Components/shared/Button.tsx
+++ frontend/dwaves-app/src/Components/shared/Button.tsx
@@ -1,4 +1,5 @@
-import styles from "@/styles/shared/styles.module.scss";
+import "styles/shared/Button.scss";
+import styles from "styles/global/styles.module.scss";
 import { Icon } from "components/shared";
 
 interface Props {
@@ -20,14 +21,47 @@ export const Button: React.FC<Props> = ({
   isDisabled = false,
   isDark = false,
 }) => {
+  const computeClassName = (): string => {
+    const sizeClass =
+      size === "Regular" ? "ds-button-regular" : "ds-button-small";
+    const colorClass =
+      color === "Negative"
+        ? "ds-button-negative"
+        : color === "Neutral"
+        ? "ds-button-neutral"
+        : "ds-button-positive";
+
+    // Those are there just in case for now
+    const isDisabledClass = isDisabled ? "ds-button-disabled" : "";
+    const isDarkClass = isDark ? "ds-button-dark" : "";
+
+    // Weird syntax, but helps with formatting and readability
+    return [
+      "ds-button",
+      "ds-flex-row-between",
+      "ds-align-self-center",
+      sizeClass,
+      colorClass,
+      isDisabledClass,
+      isDarkClass,
+    ]
+      .filter(Boolean)
+      .join(" ");
+  };
+
+  const computeTextClassName = (): string => {
+    const sizeSuffix = size === "Regular" ? "" : "-l";
+    return "ds-text-button" + sizeSuffix;
+  };
+
   return (
-    <div className="ds-button" onClick={() => handleClick()}>
+    <div className={computeClassName()} onClick={() => handleClick()}>
       {icon ? (
         <div>
           <Icon {...{ icon, size }} />
         </div>
       ) : null}
-      {text ? <div>{text}</div> : null}
+      {text ? <div className={computeTextClassName()}>{text}</div> : null}
     </div>
   );
 };
