749b54aefef2d666f721b8ecd16e06d9c2a491e6
Alex <alexandre@yakow.io>
2022-11-24 21:25:06 +0100

    feat: rm password from user in response

--- backend/api/src/services/user.service.ts
+++ backend/api/src/services/user.service.ts
@@ -3,7 +3,24 @@ import prisma from '@config/prisma.config'
 import { Prisma } from '@prisma/client'
 
 class UserService implements IUserService {
-  create = (user: Prisma.UserCreateInput) => prisma.user.create({ data: user })
+  exclude<User, Key extends keyof User>(user: User, keys: Key[]): Omit<User, Key> {
+    for (let key of keys) {
+      delete user[key]
+    }
+    return user
+  }
+
+  findMany = async () => {
+    const users = await prisma.user.findMany()
+
+    return users.map((user) => this.exclude(user, ['password']))
+  }
+
+  create = async (user: Prisma.UserCreateInput) => {
+    const createdUser = await prisma.user.create({ data: user })
+
+    return this.exclude(createdUser, ['password'])
+  }
 }
 
 const userService = new UserService()
