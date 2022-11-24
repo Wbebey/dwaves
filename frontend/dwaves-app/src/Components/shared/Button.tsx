40688ffdafb1d061d5542a2306dbccae29dd4610
Bastien FROMENT <bastien@oplit.fr>
2022-11-24 16:05:42 +0100

    fix: enable absolute imports and clean unused prop

--- frontend/dwaves-app/src/Components/shared/Button.tsx
+++ frontend/dwaves-app/src/Components/shared/Button.tsx
@@ -1,5 +1,24 @@
 import styles from "@/styles/shared/styles.module.scss";
+import { Icon } from "components/shared";
 
-export const Button = () => {
-  
+interface Props {
+  handleClick: Function;
+  size: "Regular" | "Large";
+  color: "Negative" | "Neutral" | "Positive";
+  icon?: string;
+  text?: string;
+  isDisabled: boolean;
+  isDark: boolean;
 }
+
+export const Button = ({
+  handleClick = () => {},
+  size = "Regular",
+  color = "Neutral",
+  // icon,
+  // text,
+  isDisabled = false,
+  isDark = false,
+}) => {
+  return <div className="ds-button" onClick={() => handleClick()} />;
+};
