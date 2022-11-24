fe4882d46a81af3d7467e9eb517df13d3effa8a1
TharickABDUL <abdultharick@gmail.com>
2022-11-25 00:33:48 +0100

    fix: conflit resolved Pull requests #29

--- backend/api/src/routers/album.router.ts
+++ backend/api/src/routers/album.router.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express'
 
-import AlbumController from '@controllers/album.controller'
+import albumController from '@controllers/album.controller'
 
 const albumRouter = Router()
-const albumController = new AlbumController()
 
 albumRouter.get('/', albumController.get)
 albumRouter.post('/', albumController.create)
--- backend/api/src/routers/app.router.ts
+++ backend/api/src/routers/app.router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 
 import AppController from '@controllers/app.controller'
-import userRouter from './user.router'
-import albumRouter from './album.router'
-import PinataRouter from './music.router'
+import userRouter from '@routers/user.router'
+import albumRouter from '@routers/album.router'
+import PinataRouter from '@routers/music.router'
 
 const appRouter = Router()
 const appController = new AppController()
@@ -11,7 +11,7 @@ const appController = new AppController()
 appRouter.get('/', appController.healthcheck)
 appRouter.use('/users', userRouter)
 appRouter.use('/albums', albumRouter)
-appRouter.use('/music', PinataRouter)
+appRouter.use('/musics', PinataRouter)
 
 
 export default appRouter
