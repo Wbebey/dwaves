63708281b97f577a217bdcda0e7cee9e33d7ae54
Alex <alexandre@yakow.io>
2022-11-24 21:25:06 +0100

    feat: rm password from user in response

--- backend/api/src/interfaces/service.interface.ts
+++ backend/api/src/interfaces/service.interface.ts
@@ -1,7 +1,10 @@
+import { ViewUser } from '@@types/user.type'
 import { Prisma, User } from '@prisma/client'
 
 interface IService {}
 
 export interface IUserService extends IService {
-  create: (user: Prisma.UserCreateInput) => Promise<User>
+  exclude<User, Key extends keyof User>(user: User, keys: Key[]): Omit<User, Key>
+  findMany: () => Promise<ViewUser[]>
+  create: (user: Prisma.UserCreateInput) => Promise<ViewUser>
 }
