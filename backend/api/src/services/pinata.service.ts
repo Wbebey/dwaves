e2a8aedf4506aa19ab3dda2eaca1e58319ff14fe
Alex <alexandre@yakow.io>
2022-11-26 18:15:04 +0100

    refactor: rm serviceS.interface.ts & mv types in appropriate files

--- backend/api/src/services/pinata.service.ts
+++ backend/api/src/services/pinata.service.ts
@@ -1,53 +1,31 @@
 import axios from 'axios'
-import {IPinataService} from "@interfaces/services.interface";
+import { IPinataService } from '@interfaces/service.interface'
 import FormData from 'form-data'
-import env from "@config/env.config";
-import {ReadStream} from "fs";
-
-const {pinataApiHost, pinataApiKey, pinataApiSecret} = env
-
-type PinataPinResponse = {
-    IpfsHash: string
-    PinSize: string
-    Timestamp: string
-}
-
-type Metadata = {
-    type: 'cover' | 'music'
-    artistId?: number
-    albumId?: number
-    genre?: string
-    coverCid?: string
-    listenings?: number
-}
-
-type PinataMetadata = {
-    keyvalues: Metadata
-}
+import env from '@config/env.config'
+import { ReadStream } from 'fs'
+import { IPFSMetadata, PinataPinResponse } from '@@types/pinata.type'
 
 class PinataService implements IPinataService {
-    pinFileToIPFS : (file: ReadStream, metadata: Metadata) => Promise<string> = async (file: ReadStream, metadata: Metadata) => {
-        const url = `${pinataApiHost}/pinning/pinFileToIPFS`
-        //making axios POST request to Pinata ⬇️
-        let data = new FormData();
-        data.append('file', file);
+  pinFileToIPFS = async (file: ReadStream, metadata: IPFSMetadata) => {
+    const url = `${env.pinataApiHost}/pinning/pinFileToIPFS`
 
-        const metadataPinata = JSON.stringify({
-            keyvalues: metadata
-        });
-        data.append('pinataMetadata', metadataPinata);
+    let data = new FormData()
+    const metadataPinata = JSON.stringify({ keyvalues: metadata })
+    data.append('pinataMetadata', metadataPinata)
+    data.append('file', file)
 
-        const res = await axios.post<PinataPinResponse>(url, data, {
-            headers: {
-                'Content-Type': `multipart/form-data`,
-                pinata_api_key: `${pinataApiKey}`,
-                pinata_secret_api_key: `${pinataApiSecret}`,
-            },
-        })
+    const res = await axios.post<PinataPinResponse>(url, data, {
+      headers: {
+        'Content-Type': `multipart/form-data`,
+        pinata_api_key: `${env.pinataApiKey}`,
+        pinata_secret_api_key: `${env.pinataApiSecret}`,
+      },
+    })
 
-        const coverCID = res.data.IpfsHash
-        return coverCID
-    }
+    const coverCID = res.data.IpfsHash
+
+    return coverCID
+  }
 }
 
 const pinataService = new PinataService()
