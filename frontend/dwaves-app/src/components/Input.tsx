271314e2d37436569eea86265cbae43ace1ab02b
BryanSeychelles <bryanseychellesyk@gmail.com>
2022-11-28 17:43:37 +0000

    feat: call axios upload single

--- frontend/dwaves-app/src/components/Input.tsx
+++ frontend/dwaves-app/src/components/Input.tsx
@@ -1,4 +1,4 @@
-/* import axios from "axios" */
+import axios from "axios"
 import "styles/Download.scss";
 import { Icon } from "components/shared";
 
@@ -42,14 +42,15 @@ export const Input = () => {
   }
 
   const onSubmit = (data: any) => {
-    console.log(data);
-  };
-
-  /*         useEffect(()=>{
-            axios.post(`${import.meta.env.VITE_APP_URL}`, () => {
-    
-            })
-        },[]) */
+    const form = new FormData()
+    form.append('request', JSON.stringify({ genre: data.genre, artistId: "2" }))
+    form.append("cover", data.cover)
+    form.append("music", data.src)
+    console.log(form)
+    axios.post(`${import.meta.env.VITE_APP_BACK_URL}/musics/pinSingleMusic`, form)
+      .then(res => { console.log('worked') })
+      .catch(err => { console.log(err) })
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
