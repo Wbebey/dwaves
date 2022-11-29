22fb308930015badf03ddb29b938ac892741ec8b
BryanSeychelles <bryanseychellesyk@gmail.com>
2022-11-29 21:45:54 +0000

    feat: forms to upload a album

--- frontend/dwaves-app/src/components/index.ts
+++ frontend/dwaves-app/src/components/index.ts
@@ -2,19 +2,21 @@ import { AnimateBulles } from "components/AnimateBulles";
 import { ContentAlbum } from "components/ContentAlbum";
 import { ContentPlaylist } from "components/ContentPlaylist";
 import { ExploPlayer } from "components/ExploPlayer";
-import { Input } from "components/Input";
+import { SingleForm } from "components/Input";
 import { Loader } from "components/Loader";
 import { Login } from "components/Login";
 import { PlayerReact } from "components/PlayerReact";
 import { Register } from "components/Register";
 import { Sidebar } from "components/Sidebar";
+import { AlbumForm } from "components/AlbumForm"
 
 export {
   AnimateBulles,
   ContentAlbum,
   ContentPlaylist,
   ExploPlayer,
-  Input,
+  SingleForm,
+  AlbumForm,
   Loader,
   Login,
   PlayerReact,
