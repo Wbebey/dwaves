ac6c36ec3be425bdb2244b9d1735ea59af2949b6
TharickABDUL <abdultharick@gmail.com>
2022-11-30 15:41:57 +0100

    feat: can get all music from pinata without filters

--- backend/api/src/types/pinata.type.ts
+++ backend/api/src/types/pinata.type.ts
@@ -15,8 +15,23 @@ export type MusicMetadata = {
   listenings: number
 }
 
+export type MusicWithMetadata = {
+  musicName: string
+  musicUrl: string
+  album: string
+  coverUrl: string
+  genre: string
+  artist: string
+  listenings: number
+}
+
 export type PinataPinResponse = {
   IpfsHash: string
   PinSize: string
   Timestamp: string
+}
+
+
+export type PinataGetResponse = {
+  rows: any
 }
