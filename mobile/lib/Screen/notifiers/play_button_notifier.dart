0d65b10632f4a9ccd6c57974c081014ac1a1131d
sofian <sofiansalh2305@gmil.com>
2022-12-01 10:45:47 +0100

    feat: player design

---
+++ mobile/lib/Screen/notifiers/play_button_notifier.dart
@@ -0,0 +1,9 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class PlayButtonNotifier extends ValueNotifier<ButtonState> {
+  PlayButtonNotifier() : super(_initialValue);
+  static const _initialValue = ButtonState.paused;
+}
+
+enum ButtonState { paused, playing, loading, }
