b4754f575d9563b827e438c9b626fbf90a940d39
sofian <sofiansalh2305@gmil.com>
2022-12-01 10:45:47 +0100

    feat: player design

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,11 @@
 
-
-import 'package:dwaves_mobile/Screen/player.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:dwaves_mobile/Componant/Boutton.dart';
 import 'Componant/Boutton.dart';
+
+import 'package:dwaves_mobile/Screen/manager.dart';
+
 
 
 void main() => runApp(MyApp());
@@ -12,10 +13,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Login_Page(),
+    return MaterialApp(
+    
+      home: Manager(),
     );
   }
 }
+
 // check connection wallet flutter 
