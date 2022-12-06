cb5bdc000c2343c422a31c0e0daf305bf01e0ff8
BryanSeychelles <bryanseychellesyk@gmail.com>
2022-12-06 16:29:59 +0000

    feat: page ico with design for dekstop created

---
+++ frontend/dwaves-website/src/components/svg/Wave.tsx
@@ -0,0 +1,9 @@
+import "../../styles/svg/Wave.scss"
+
+export const Wave = () => {
+    return (
+        <svg className="wave ico w-full" viewBox="0 0 1920 396" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 1.52588e-05L960 0L1920 1.52588e-05V155C1920 155 1889 235 1536 220C1183 205 742 115 806.5 155C922.811 227.131 975.5 658 0 155V1.52588e-05Z" fill="#475BFB" />
+        </svg>
+    )
+}
