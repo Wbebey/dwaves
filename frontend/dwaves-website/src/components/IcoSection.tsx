6b00dd77a0e11a02d233c4319964dcae99ef3530
BryanSeychelles <bryanseychellesyk@gmail.com>
2022-12-07 14:56:57 +0000

    fix: delete value timer and children props in App.tsx

--- frontend/dwaves-website/src/components/IcoSection.tsx
+++ frontend/dwaves-website/src/components/IcoSection.tsx
@@ -20,25 +20,25 @@ export const IcoSection = () => {
                 <div className="grid grid-flow-col gap-5 text-center auto-cols-max mx-auto">
                     <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                         <span className="countdown font-mono text-9xl">
-                            <span style={{ "--value": 15 }}></span>
+                            <span>14</span>
                         </span>
                         days
                     </div>
                     <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                         <span className="countdown font-mono text-9xl">
-                            <span style={{ "--value": 10 }}></span>
+                            <span>14</span>
                         </span>
                         hours
                     </div>
                     <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                         <span className="countdown font-mono text-9xl">
-                            <span style={{ "--value": 24 }}></span>
+                            <span>14</span>
                         </span>
                         min
                     </div>
                     <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
                         <span className="countdown font-mono text-9xl">
-                            <span style={{ "--value": 51 }}></span>
+                            <span>14</span>
                         </span>
                         sec
                     </div>
