b8f5358dadf8376e992c0f1968725dee84bf27c1
Alex <alexandre@yakow.io>
2022-12-08 10:59:44 +0100

    feat: cookie sameSite none

--- backend/api/src/controllers/auth.controller.ts
+++ backend/api/src/controllers/auth.controller.ts
@@ -12,14 +12,14 @@ const accessTokenCookieOptions: CookieOptions = {
   expires: new Date(Date.now() + env.accessTokenExp * 60 * 1000),
   maxAge: env.accessTokenExp * 60 * 1000,
   httpOnly: true,
-  sameSite: 'lax',
+  sameSite: 'none',
 }
 
 const refreshTokenCookieOptions: CookieOptions = {
   expires: new Date(Date.now() + env.refreshTokenExp * 60 * 1000),
   maxAge: env.refreshTokenExp * 60 * 1000,
   httpOnly: true,
-  sameSite: 'lax',
+  sameSite: 'none',
 }
 
 if (process.env.NODE_ENV === 'production') {
