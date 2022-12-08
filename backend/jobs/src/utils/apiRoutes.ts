90c5fdda648206767d31cb9e83729e62e517ba55
Alex <alexandre@yakow.io>
2022-12-08 01:57:55 +0100

    fix: apiHost & auth

--- backend/jobs/src/utils/apiRoutes.ts
+++ backend/jobs/src/utils/apiRoutes.ts
@@ -2,20 +2,21 @@ import { checkEnv } from './checkEnv'
 
 const config = {
   apiHost: checkEnv('API_HOST'),
-  apiPort: checkEnv('API_PORT'),
 }
 
 type ApiRoutes = {
+  LOGIN: string
   MONTHLY_LISTENINGS: string
 }
 
 const routes: ApiRoutes = {
+  LOGIN: '/auth/login',
   MONTHLY_LISTENINGS: '/users/monthlyListenings',
 }
 
 Object.keys(routes).forEach((r) => {
   const route = r as keyof ApiRoutes
-  routes[route] = `${config.apiHost}:${config.apiPort}${routes[route]}`
+  routes[route] = `${config.apiHost}${routes[route]}`
 })
 
 export default routes
