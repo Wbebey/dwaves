af82c664189d723c8012ec2cbef8975873c8af2b
Alex <alexandre@yakow.io>
2022-12-08 03:24:09 +0100

    feat: don't reload on login/logout

--- frontend/dwaves-app/src/App.tsx
+++ frontend/dwaves-app/src/App.tsx
@@ -1,72 +1,60 @@
-import "./App.scss";
-import { Loader, ExploPlayer, Sidebar } from "components";
-import { Player, Explorer, Album, Download, ModalLogin } from "views";
+import './App.scss'
+import { Loader, ExploPlayer, Sidebar } from 'components'
+import { Player, Explorer, Album, Download, ModalLogin } from 'views'
 
-import { useEffect, useRef, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useRef, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import datasong from "songs/datasongs";
+import datasong from 'songs/datasongs'
 
 function App() {
-  const [loader, setLoader] = useState(true);
-  const [songs, setSongs] = useState(datasong);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [currentSong, setCurrentSong] = useState(datasong[0]);
-  const [loginDisplay, setLoginDisplay] = useState(false);
+  const [loader, setLoader] = useState(true)
+  const [songs, setSongs] = useState(datasong)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [currentSong, setCurrentSong] = useState(datasong[0])
+  const [loginDisplay, setLoginDisplay] = useState(false)
   // Temporary this value will be stored in the token
   const [connected, setConnected] = useState(false)
 
-  const audioElmt = useRef<HTMLAudioElement>(null) ?? someOtherData();
-
-  const log = document.cookie.split('=')[1]
+  const audioElmt = useRef<HTMLAudioElement>(null) ?? someOtherData()
 
   useEffect(() => {
     setTimeout(() => {
-      setLoader(false);
-    }, 3000);
+      setLoader(false)
+    }, 1000)
 
     if (isPlaying) {
-      audioElmt.current?.play();
+      audioElmt.current?.play()
     } else {
-      audioElmt.current?.pause();
+      audioElmt.current?.pause()
     }
+  }, [audioElmt, isPlaying])
 
-    if (log == "true") {
+  useEffect(() => {
+    if (document.cookie === 'loggedIn=true') {
       setConnected(true)
-    } else {
-      setConnected(false)
     }
-
-  }, [audioElmt, isPlaying]);
+  }, [])
 
   const onPlaying = () => {
-    const duration: number = audioElmt.current?.duration as number;
-    const ct: number = audioElmt.current?.currentTime as number;
+    const duration: number = audioElmt.current?.duration as number
+    const ct: number = audioElmt.current?.currentTime as number
 
     setCurrentSong({
       ...currentSong,
       progress: (ct / duration) * 100,
       length: duration,
-    });
-  };
+    })
+  }
 
-  const displayModal = (e: any) => {
-    switch (loginDisplay) {
-      case true:
-        setLoginDisplay(false);
-        break;
-      case false:
-        setLoginDisplay(true);
-        break;
-      default:
-        break;
-    }
-  };
+  const toggleModal = () => {
+    setLoginDisplay(!loginDisplay)
+  }
 
   return loader ? (
     <Loader />
   ) : (
-    <section style={{ color: "black", height: window.innerHeight }}>
+    <section style={{ color: 'black', height: window.innerHeight }}>
       <audio src={currentSong.Src} ref={audioElmt} onTimeUpdate={onPlaying} />
       <ExploPlayer
         audioElmt={audioElmt}
@@ -77,12 +65,12 @@ function App() {
         songs={songs}
         setSongs={setSongs}
       />
-      <section style={{ color: "black", height: "75%" }}>
+      <section style={{ color: 'black', height: '75%' }}>
         <section className="container-app">
           <div className="contain-explorer">
             <Router>
               <Sidebar
-                displayModal={displayModal}
+                toggleModal={toggleModal}
                 connected={connected}
                 setConnected={setConnected}
               />
@@ -99,12 +87,16 @@ function App() {
           </div>
         </section>
       </section>
-      {loginDisplay ? <ModalLogin displayModal={displayModal} /> : <div />}
+      {loginDisplay ? (
+        <ModalLogin toggleModal={toggleModal} setConnected={setConnected} />
+      ) : (
+        <div />
+      )}
     </section>
-  );
+  )
 }
 
-export default App;
-function someOtherData(): import("react").RefObject<HTMLAudioElement> {
-  throw new Error("Function not implemented.");
+export default App
+function someOtherData(): import('react').RefObject<HTMLAudioElement> {
+  throw new Error('Function not implemented.')
 }
--- frontend/dwaves-app/src/components/Login.tsx
+++ frontend/dwaves-app/src/components/Login.tsx
@@ -1,33 +1,51 @@
-import axios from "axios";
-import { useForm } from "react-hook-form";
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
 type User = {
-  username: string;
-  email: string;
-  password: string;
-};
+  username: string
+  email: string
+  password: string
+}
 
-export const Login = () => {
-  const { register, setValue, getValues, handleSubmit } = useForm<User>();
+interface Props {
+  toggleModal: () => void
+  setConnected: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const Login: React.FC<Props> = ({ toggleModal, setConnected }) => {
+  const { register, setValue, getValues, handleSubmit } = useForm<User>()
 
   const onSubmit = (data: any) => {
-    axios.post(`${import.meta.env.VITE_APP_BACK_URL}/auth/login`, data, {withCredentials: true})
-      .then(res => { 
-        // window.location.reload()
+    axios
+      .post(`${import.meta.env.VITE_APP_BACK_URL}/auth/login`, data, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (document.cookie === 'loggedIn=true') {
+          setConnected(true)
+          toggleModal()
+        } else {
+          alert('COOKIE NOT SET')
+        }
         console.log(res)
       })
-      .catch(err => {console.log(err)})
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col justify-center"
+    >
       <div id="input-text" className="form-control w-full">
         <label className="label">
           <span className="label-text">Username</span>
         </label>
         <input
           type="email"
-          {...register("email")}
+          {...register('email')}
           placeholder="Type here"
           className="input input-ghost"
         />
@@ -40,12 +58,12 @@ export const Login = () => {
           type="password"
           placeholder="Type here"
           className="input input-ghost"
-          {...register("password")}
+          {...register('password')}
         />
       </div>
       <button type="submit" className="btn btn-primary m-8">
         Login
       </button>
     </form>
-  );
-};
+  )
+}
