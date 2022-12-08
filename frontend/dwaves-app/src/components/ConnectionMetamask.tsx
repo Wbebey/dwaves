6edd693c389ae75d13c5b376f626383be5623035
BryanSeychelles <bryanseychellesyk@gmail.com>
2022-12-08 00:01:37 +0000

    fix: test willy

--- frontend/dwaves-app/src/components/ConnectionMetamask.tsx
+++ frontend/dwaves-app/src/components/ConnectionMetamask.tsx
@@ -1,3 +1,5 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 declare const window: Window &
     typeof globalThis & {
@@ -6,19 +8,41 @@ declare const window: Window &
 
 export const ConnectMetamask = () => {
 
+    const [wallet, setWallet] = useState()
+
     const requestConnection = async () => {
         if (window.ethereum) {
             const accounts = await window.ethereum.request({
                 method: "eth_requestAccounts",
             })
+            setWallet(accounts)
         }
     }
+
+    const RequestAdress = async (data:any) => {
+        const request = await axios.put(`${import.meta.env.VITE_APP_BACK_URL}/users/me/addWallet`, data, { withCredentials: true })
+    }
+
+    useEffect(() => {
+        if (wallet) {
+            console.log('connected')
+            console.log(wallet)
+            const data = { address: wallet[0] }
+            RequestAdress(data)
+        }
+    }, [wallet])
 
     return (
         <li>
             <div onClick={requestConnection} className="avatar mx-auto">
                 <div className="w-8 rounded-full">
-                    <img src={`${import.meta.env.VITE_APP_URL}/metamask.png`} alt='' />
+                    {
+                        wallet ?
+                            <img src={`${import.meta.env.VITE_APP_URL}metamask-b&w.png`} alt='' />
+                            :
+                            <img src={`${import.meta.env.VITE_APP_URL}metamask-color.png`} alt='' />
+                    }
+
                 </div>
             </div>
         </li>
