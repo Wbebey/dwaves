933cee50eaad9c8d489fd516ab294cdc774f6e27
TharickABDUL <abdultharick@gmail.com>
2023-03-02 11:07:58 +0100

    feat: init profile page

---
+++ frontend/dwaves-app/src/views/Profile.tsx
@@ -0,0 +1,10 @@
+import "styles/Explorer.scss";
+import {PopularSongOfArtist} from "../components";
+
+export const Profile = () => {
+    return (
+        <div className={'w-full'}>
+            <PopularSongOfArtist/>
+        </div>
+    )
+};
