b947be92366cef4a246c43389974416cf395f5af
TharickABDUL <abdultharick@gmail.com>
2023-03-03 11:28:44 +0100

    feat: list MostPoular song v1

--- frontend/dwaves-app/src/views/Profile.tsx
+++ frontend/dwaves-app/src/views/Profile.tsx
@@ -1,10 +1,21 @@
 import "styles/Explorer.scss";
-import {PopularSongOfArtist} from "../components";
+import {AlbumForm, PopularSongOfArtist, SingleForm, SwitchTab} from "../components";
+import {useState} from "react";
 
 export const Profile = () => {
+
+    const [showForm, setShowForm] = useState(true)
+
     return (
-        <div className={'w-full'}>
-            <PopularSongOfArtist/>
+        <div className="contain-download" style={{width: '100%', height: '100%', background: 'white'}}>
+
+            <SwitchTab FirstValue={'Overview'} SecondValue={'My Info'} showForm={showForm} setShowForm={setShowForm}/>
+
+            {showForm ?
+                <PopularSongOfArtist/>
+                :
+                null
+            }
         </div>
     )
 };
