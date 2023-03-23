5e534830b17e9d5d3e29b585369259023acabc39
TharickABDUL <abdultharick@gmail.com>
2023-03-23 16:14:35 +0100

    feat: can create Playlist

--- frontend/dwaves-app/src/components/index.ts
+++ frontend/dwaves-app/src/components/index.ts
@@ -14,6 +14,7 @@ import { PopularSongOfArtist} from "components/PopularSongOfArtist";
 import { AlbumOfArtist } from "components/AlbumOfArtist";
 import { SwitchTab } from "components/SwitchTab";
 import { SongList } from "components/SongList";
+import { CreatePlaylist } from "components/CreatePlaylist";
 import { Footer } from "components/Footer";
 
 export {
@@ -33,5 +34,6 @@ export {
   AlbumOfArtist,
   SwitchTab,
   SongList,
+  CreatePlaylist,
   Footer
 };
--- frontend/dwaves-app/src/views/index.ts
+++ frontend/dwaves-app/src/views/index.ts
@@ -4,5 +4,6 @@ import { Explorer } from "views/Explorer";
 import { Profile } from "views/Profile";
 import { ModalLogin } from "views/ModalLogin";
 import { Player } from "views/Player";
+import Playlist from "./Playlist";
 
-export { Album, Download, Explorer, ModalLogin, Player, Profile };
+export { Album, Download, Explorer, ModalLogin, Player, Profile, Playlist };
