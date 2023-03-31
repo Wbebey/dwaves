2fe1a23c3db6de7f120a6ce0bd7f6a7e5f5e5784
Bastien FROMENT <bastien@oplit.fr>
2023-03-31 16:48:44 +0200

    fix: remove glslify module

--- frontend/dwaves-app/src/glslify.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.glsl" {
-  const value: string;
-  export default value;
-}
-
-declare module "glslify" {
-  function glslify(source: string, options?: object): string;
-  export = glslify;
-}
