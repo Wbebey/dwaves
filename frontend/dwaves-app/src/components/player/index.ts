2edb135dbdc93f3ec4b5dcb9220aacee9b957208
Bastien FROMENT <bastien@oplit.fr>
2023-03-31 16:46:54 +0200

    fix: remove PlayerExplorer

--- frontend/dwaves-app/src/components/player/index.ts
+++ frontend/dwaves-app/src/components/player/index.ts
@@ -1,6 +1,5 @@
 import { PlayerShader } from 'components/player/PlayerShader'
-import { PlayerExplorer } from 'components/player/PlayerExplorer'
 import { PlayerReact } from 'components/player/PlayerReact'
 import { PlayerWrapper } from 'components/player/PlayerWrapper'
 
-export { PlayerShader, PlayerExplorer, PlayerReact, PlayerWrapper }
+export { PlayerShader, PlayerReact, PlayerWrapper }
