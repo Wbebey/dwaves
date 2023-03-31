78a4c2c930ae097d67d3a0b93229dbda6c7335c4
Bastien FROMENT <bastien@oplit.fr>
2023-03-31 12:12:32 +0200

    fix: linter

--- frontend/dwaves-app/src/components/player/index.ts
+++ frontend/dwaves-app/src/components/player/index.ts
@@ -1,6 +1,6 @@
-import { PlayerShader } from "components/player/PlayerShader";
-import { PlayerExplorer } from "components/player/PlayerExplorer";
-import { PlayerReact } from "components/player/PlayerReact";
-import { PlayerWrapper } from "components/player/PlayerWrapper";
+import { PlayerShader } from 'components/player/PlayerShader'
+import { PlayerExplorer } from 'components/player/PlayerExplorer'
+import { PlayerReact } from 'components/player/PlayerReact'
+import { PlayerWrapper } from 'components/player/PlayerWrapper'
 
-export { PlayerShader, PlayerExplorer, PlayerReact, PlayerWrapper };
+export { PlayerShader, PlayerExplorer, PlayerReact, PlayerWrapper }
