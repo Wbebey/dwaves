9aece5efb0cff465cdbeabe550b74735da2b6986
Alexandre Pinon <alexandre@yakow.io>
2023-04-14 15:24:15 +0200

    feat: create concert event route

--- backend/api/src/config/alchemy.config.ts
+++ backend/api/src/config/alchemy.config.ts
@@ -3,7 +3,7 @@ import { Alchemy, Network } from 'alchemy-sdk'
 import { Wallet } from 'ethers'
 
 const getWallet = async (privateKey: string): Promise<Wallet> => {
-  const settings = { apiKey: env.alchemyApiKey, network: Network.ETH_GOERLI }
+  const settings = { apiKey: env.alchemyApiKey, network: Network.ETH_SEPOLIA }
   const alchemy = new Alchemy(settings)
   const alchemyProvider = await alchemy.config.getProvider()
   const dwavesPayer = new Wallet(privateKey, alchemyProvider)
