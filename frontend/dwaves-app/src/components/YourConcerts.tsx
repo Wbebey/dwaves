bfe23ffdff9186e43623f073a7d9c7ad055984ff
TharickABDUL <abdultharick@gmail.com>
2023-04-26 17:26:21 +0200

    feat: can buy NFT ticket from dwaves-app

--- frontend/dwaves-app/src/components/YourConcerts.tsx
+++ frontend/dwaves-app/src/components/YourConcerts.tsx
@@ -19,14 +19,14 @@ const TheEvent = ({ event }: any) => {
     <div className="card bg-gray-50 border mx-2 my-4 hover:bg-teal-300">
       <div className="absolute pl-5 pt-3 w-80">
         <p className="text-3xl text-white font-bold">{event.name}</p>
-        <p className="pt-3 text-white">{event.date}</p>
+        <p className="pt-3 text-white text-xl">{event.date}</p>
         {/*<p>{event.daysUntilConcert}</p>*/}
-        <p className="pt-1 text-white font-bold text-xl">{event.place}</p>
-        <p className="pt-1 text-white font-bold text-xl">{event.genre}</p>
-        <p className="pt-1 text-cyan-100 text-xl">
+        <p className="pt-1 text-white text-xl">{event.place}</p>
+        <p className="pt-1 text-white text-xl">{event.genre}</p>
+        <p className="pt-1 text-white text-xl">
           {event.ticketSold}/{event.totalTickets} tickets sold
         </p>
-        <p className="pt-1 text-cyan-100 text-2xl font-bold">
+        <p className="pt-1 text-white text-2xl font-bold">
           {event.price} Vibes
         </p>
       </div>
