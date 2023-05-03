bf35066bc7723d8c1c21d61372674d7699c79645
TharickABDUL <abdultharick@gmail.com>
2023-05-03 22:03:34 +0200

    feat: adding app information in settings page

--- frontend/dwaves-app/src/views/index.ts
+++ frontend/dwaves-app/src/views/index.ts
@@ -1,11 +1,23 @@
-import { Album } from "views/Album";
-import { Download } from "views/Download";
-import { Explorer } from "views/Explorer";
-import { Profile } from "views/Profile";
-import { ModalLogin } from "views/ModalLogin";
-import { Player } from "views/Player";
-import { PlaylistPage } from "./PlaylistPage";
-import { Playlist } from "./Playlist";
-import { Marketplace } from "./Marketplace";
+import { Album } from 'views/Album'
+import { Download } from 'views/Download'
+import { Explorer } from 'views/Explorer'
+import { Profile } from 'views/Profile'
+import { ModalLogin } from 'views/ModalLogin'
+import { Player } from 'views/Player'
+import { PlaylistPage } from './PlaylistPage'
+import { Playlist } from './Playlist'
+import { Marketplace } from './Marketplace'
+import { Settings } from './Settings'
 
-export { Album, Download, Explorer, ModalLogin, Player, Profile, PlaylistPage, Playlist, Marketplace };
+export {
+  Album,
+  Download,
+  Explorer,
+  ModalLogin,
+  Player,
+  Profile,
+  PlaylistPage,
+  Playlist,
+  Marketplace,
+  Settings,
+}
