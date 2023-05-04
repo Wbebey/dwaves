1a55f1120f7ab6ebdba65b9415e7a13d2425055e
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: update jobs to new architecture (call api instead of db)"
    This reverts commit b202d7842ed742c9dce389f4282ceafd351877ae.

--- backend/jobs/src/utils/checkEnv.ts
+++
@@ -1,7 +0,0 @@
-export const checkEnv = (variable: string): string => {
-  const envVariable = process.env[variable]
-  if (!envVariable) {
-    throw new Error(`Couldn't find environment variable: ${variable}`)
-  }
-  return envVariable
-}
