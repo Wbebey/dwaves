7f35e0deb47dc5a7d9e642d192d9406f4de21150
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat(payArtists): retrieve users monthly listenings + update prisma schema"
    This reverts commit b91de47a84128dd467e539fac55b95ce5b2bfd2c.

--- backend/jobs/payArtists.ts
+++ backend/jobs/payArtists.ts
@@ -1,50 +1,5 @@
 import { HttpFunction } from '@google-cloud/functions-framework'
-import { PrismaClient } from '@prisma/client'
 
-export const payArtists: HttpFunction = async (req, res) => {
-  const prisma = new PrismaClient()
-
-  const now = new Date()
-  now.setHours(0, 0, 0, 0)
-  const oneMonthBefore = new Date(
-    new Date(now.getTime()).setMonth(now.getMonth() - 1)
-  )
-
-  const artists = await prisma.user.findMany({
-    //* Find users with at least one record in MonthlyListenings for the previous month
-    where: {
-      monthlyListenings: {
-        some: {
-          date: {
-            gte: oneMonthBefore,
-            lt: now,
-          },
-        },
-      },
-    },
-    select: {
-      address: true,
-      monthlyListenings: {
-        //* Select ONLY the MonthlyListenings of the previous month
-        where: {
-          date: {
-            gte: oneMonthBefore,
-            lt: now,
-          },
-        },
-        select: {
-          listenings: true,
-        },
-      },
-    },
-  })
-
-  const listenings = artists.map((a) =>
-    a.monthlyListenings
-      .map((ml) => ml.listenings)
-      .reduce((prev, curr) => prev + curr)
-  )
-  const artistAddresses = artists.map((a) => a.address)
-
-  res.json({ listenings, artistAddresses })
+export const payArtists: HttpFunction = (req, res) => {
+  res.send('Hello, World')
 }
