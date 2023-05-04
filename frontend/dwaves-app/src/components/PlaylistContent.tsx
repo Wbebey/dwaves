8bd343c297aa4b3e2f2b2b24dcde326ae436527a
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: display the liked musics playlist in the playlist page"
    This reverts commit 4189c527a8d39b2218c76963612ae1d4f1267960.

--- frontend/dwaves-app/src/components/PlaylistContent.tsx
+++ frontend/dwaves-app/src/components/PlaylistContent.tsx
@@ -1,183 +1,153 @@
-import "styles/ContentAlbum.scss";
-import { Icon } from "components/shared";
-import { Link, useParams } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { SongList } from "./SongList";
-import { AlbumDetail, responseRequest } from "../models";
+import 'styles/ContentAlbum.scss'
+import {Icon} from 'components/shared'
+import {Link, useParams} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {SongList} from "./SongList";
+import {AlbumDetail, responseRequest} from "../models";
 
 interface Props {
-  setCurrentSong: React.Dispatch<React.SetStateAction<any>>;
-  setSongs: React.Dispatch<React.SetStateAction<any>>;
-  audioElmt: React.RefObject<HTMLAudioElement>;
-  isPlaying: boolean;
-  setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
-  setArtist: React.Dispatch<React.SetStateAction<AlbumDetail | undefined>>;
-  setAlert: React.Dispatch<React.SetStateAction<responseRequest | undefined>>;
-  likedMusics: string[];
-  likeOrDislikeMusic: (music: string) => void;
+    setCurrentSong: React.Dispatch<React.SetStateAction<any>>
+    setSongs: React.Dispatch<React.SetStateAction<any>>
+    audioElmt: React.RefObject<HTMLAudioElement>;
+    isPlaying: boolean;
+    setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+    setArtist : React.Dispatch<React.SetStateAction<AlbumDetail|undefined>>;
+    setAlert: React.Dispatch<React.SetStateAction<responseRequest | undefined>>
+    likedMusics: string[];
+    likeOrDislikeMusic: (music: string)=> void
 }
 
 export const PlaylistContent: React.FC<Props> = ({
-  setCurrentSong,
-  setSongs,
-  audioElmt,
-  isPlaying,
-  setIsPlaying,
-  setArtist,
-  setAlert,
-  likedMusics,
-  likeOrDislikeMusic,
-}) => {
-  const { id } = useParams();
+                                                       setCurrentSong,
+                                                       setSongs,
+                                                       audioElmt,
+                                                       isPlaying,
+                                                       setIsPlaying,
+                                                       setArtist,
+                                                       setAlert,
+                                                       likedMusics,
+                                                       likeOrDislikeMusic
+                                                   }) => {
+    const {id} = useParams()
 
-  const [playlist, setPlaylist] = useState<any>();
-  const [isLikedMusicsPlaylist, setIsLikedMusicsPlaylist] =
-    useState<boolean>(false);
+    const [playlist, setPlaylist] = useState<any>()
 
-  const getPlaylistDetails = async (id: string) => {
-    try {
-      const res = await axios.get(
-        `${import.meta.env.VITE_APP_BACK_URL}/playlists/${id}`,
-        {
-          withCredentials: true,
+    const getPlaylistDetails = async (id: string) => {
+        try {
+            const res = await axios.get(
+                `${import.meta.env.VITE_APP_BACK_URL}/playlists/${id}`,
+                {
+                    withCredentials: true,
+                }
+            )
+
+            const resPlaylist = res.data
+
+            const coverUrl = `${import.meta.env.VITE_PINATA_GATEWAY_HOST}/${res.data.coverCID}`
+
+            const thePlaylist = {...resPlaylist, cover: coverUrl, playlistId: id}
+
+
+            console.log(thePlaylist)
+            setPlaylist(thePlaylist)
+        } catch (error) {
+            console.log(error)
         }
-      );
-
-      const resPlaylist = res.data;
-      const coverUrl = `${import.meta.env.VITE_PINATA_GATEWAY_HOST}/${
-        res.data.coverCID
-      }`;
-      const thePlaylist = { ...resPlaylist, cover: coverUrl, playlistId: id };
-      console.log(thePlaylist);
-      setPlaylist(thePlaylist);
-    } catch (error) {
-      console.log(error);
     }
-  };
 
-  const deleteMusicToThePlaylist = async (musicSrc: string) => {
-    let musicsCid: string[] | undefined = [];
-    try {
-      const res = await axios.get(
-        `${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlist.id}`,
-        {
-          withCredentials: true,
+
+    const deleteMusicToThePlaylist = async (musicSrc: string) => {
+        console.log('hey hey')
+        let musicsCid: string[] | undefined = []
+        try {
+
+            const res = await axios.get(`${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlist.id}`, {
+                    withCredentials: true,
+                }
+            )
+
+            res.data.musics.forEach((music: any) => {
+                const musicUrl = music.src.split('/')
+                musicsCid!.push(musicUrl[musicUrl.length - 1])
+            })
+
+            const newMusicUrl = musicSrc.split('/')
+            const newMusicCid = newMusicUrl[newMusicUrl.length - 1]
+
+            let index = musicsCid!.indexOf(newMusicCid);
+
+            if (index !== -1) {
+                musicsCid!.splice(index, 1);
+            }
+
+            const data = {musics: musicsCid}
+            const res2 = await axios.put(`${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlist.id}`, data, {
+                    withCredentials: true,
+                }
+            )
+
+            if (id) {
+                getPlaylistDetails(id)
+            }
+            if (Array.isArray(res2.data)) {
+                displayAlert(res2.data[0].msg, res2.status)
+            } else {
+                displayAlert('Music deleted successfully from the playlist', res2.status)
+            }
+        } catch (err) {
+            console.log(err)
         }
-      );
+    }
 
-      res.data.musics.forEach((music: any) => {
-        const musicUrl = music.src.split("/");
-        musicsCid!.push(musicUrl[musicUrl.length - 1])
-      });
-
-      const newMusicUrl = musicSrc.split("/");
-      const newMusicCid = newMusicUrl[newMusicUrl.length - 1];
-
-      let index = musicsCid!.indexOf(newMusicCid);
-
-      if (index !== -1) {
-        musicsCid!.splice(index, 1);
-      }
-
-      const data = { musics: musicsCid };
-      const res2 = await axios.put(
-        `${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlist.id}`,
-        data,
-        {
-          withCredentials: true,
+    useEffect(() => {
+        if (id) {
+            getPlaylistDetails(id)
         }
-      );
+    }, [id])
 
-      if (id) {
-        getPlaylistDetails(id);
-      }
-      if (Array.isArray(res2.data)) {
-        displayAlert(res2.data[0].msg, res2.status);
-      } else {
-        displayAlert(
-          "Music deleted successfully from the playlist",
-          res2.status
-        );
-      }
-    } catch (err) {
-      console.log(err);
+
+    const displayAlert = (msg: string, status: number) => {
+        setAlert({response: msg, status: status, visible: true})
+        setTimeout(() => {
+            setAlert({response: "", status: 0, visible: false})
+        }, 3000)
     }
-  };
 
-  const getLikedMusics = async () => {
-    try {
-      const res = await axios.get(
-        `${import.meta.env.VITE_APP_BACK_URL}/users/me/likedMusics`,
-        {
-          withCredentials: true,
-        }
-      );
-      console.log(res.data);
-      setIsLikedMusicsPlaylist(true);
-      setPlaylist({
-        name: "My liked musics",
-        cover: "/playlistLiked.webp",
-        musics: res.data,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    if (id !== "likedMusicsPlaylist") {
-      getPlaylistDetails(id!);
-    } else {
-      getLikedMusics();
-    }
-  }, [id]);
-
-  const displayAlert = (msg: string, status: number) => {
-    setAlert({ response: msg, status: status, visible: true });
-    setTimeout(() => {
-      setAlert({ response: "", status: 0, visible: false });
-    }, 3000);
-  };
-
-  return (
-    <div className="content-album">
-      <header className="head">
-        <Link to={"/"}>
-          <Icon icon="return" size="Large" />
-        </Link>
-        <div className="divider divider-horizontal" />
-        <div className="title">
-          {playlist && (
-            <>
-              <img src={playlist.cover} alt="" />
-              {/*<img src={`${import.meta.env.VITE_PINATA_GATEWAY_HOST}/${playlist.coverCID}`} alt=""/>*/}
-              <h3>{playlist?.name}</h3>
-            </>
-          )}
+    return (
+        <div className="content-album">
+            <header className="head">
+                <Link to={'/'}>
+                    <Icon icon="return" size="Large"/>
+                </Link>
+                <div className="divider divider-horizontal"/>
+                <div className="title">
+                    {playlist &&
+                        <>
+                            <img src={`${import.meta.env.VITE_PINATA_GATEWAY_HOST}/${playlist.coverCID}`} alt=""/>
+                            <h3>{playlist?.name}</h3>
+                        </>
+                    }
+                </div>
+                <div className="avatar">
+                    <div className="w-14 h-14 rounded-full">
+                        <img src="https://placeimg.com/192/192/people" alt=""/>
+                    </div>
+                </div>
+            </header>
+            <SongList songs={playlist}
+                      setSongs={setSongs}
+                      setCurrentSong={setCurrentSong}
+                      audioElmt={audioElmt}
+                      isPlaying={isPlaying}
+                      setIsPlaying={setIsPlaying}
+                      setArtist={setArtist}
+                      deleteMusicToThePlaylist={deleteMusicToThePlaylist}
+                      isPlaylistSong={true}
+                      setAlert={setAlert}
+                      likedMusics={likedMusics}
+                      likeOrDislikeMusic={likeOrDislikeMusic}
+            />
         </div>
-        <div className="avatar">
-          <div className="w-14 h-14 rounded-full">
-            <img src="https://placeimg.com/192/192/people" alt="" />
-          </div>
-        </div>
-      </header>
-      <SongList
-        songs={playlist}
-        setSongs={setSongs}
-        setCurrentSong={setCurrentSong}
-        audioElmt={audioElmt}
-        isPlaying={isPlaying}
-        setIsPlaying={setIsPlaying}
-        setArtist={setArtist}
-        deleteMusicToThePlaylist={deleteMusicToThePlaylist}
-        isPlaylistSong={true}
-        isLikedPlaylist={isLikedMusicsPlaylist}
-        setAlert={setAlert}
-        likedMusics={likedMusics}
-        likeOrDislikeMusic={likeOrDislikeMusic}
-        setPlaylist={setPlaylist}
-      />
-    </div>
-  );
-};
+    )
+}
--- frontend/dwaves-app/src/components/SongList.tsx
+++ frontend/dwaves-app/src/components/SongList.tsx
@@ -1,236 +1,193 @@
-import { PlayPause } from "songs/listenMusic";
-import { AlbumDetail, responseRequest, Playlists } from "../models";
-import { Icon } from "./shared";
-import React, { useEffect, useState } from "react";
+import {PlayPause} from "songs/listenMusic";
+import {AlbumDetail, responseRequest, Playlists} from "../models";
+import {Icon} from "./shared";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
-import { IoAdd } from "react-icons/all";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import playlist from "../songs/playlist";
+import {IoAdd} from "react-icons/all";
+import {BsThreeDotsVertical} from "react-icons/bs";
 
 interface Props {
-  songs: AlbumDetail;
-  setCurrentSong: React.Dispatch<React.SetStateAction<any>>;
-  setSongs: React.Dispatch<React.SetStateAction<any>>;
-  audioElmt: React.RefObject<HTMLAudioElement>;
-  isPlaying: boolean;
-  setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
-  setArtist: React.Dispatch<React.SetStateAction<AlbumDetail | undefined>>;
-  isPlaylistSong?: boolean;
-  isLikedPlaylist?: boolean;
-  setPlaylist?: React.Dispatch<any>;
-  deleteMusicToThePlaylist?: any;
-  setAlert: React.Dispatch<React.SetStateAction<responseRequest | undefined>>;
-  likedMusics: string[];
-  likeOrDislikeMusic: (music: string) => void;
+    songs: AlbumDetail
+    setCurrentSong: React.Dispatch<React.SetStateAction<any>>
+    setSongs: React.Dispatch<React.SetStateAction<any>>
+    audioElmt: React.RefObject<HTMLAudioElement>
+    isPlaying: boolean
+    setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>
+    setArtist: React.Dispatch<React.SetStateAction<AlbumDetail | undefined>>
+    isPlaylistSong?: boolean
+    deleteMusicToThePlaylist?: any
+    setAlert: React.Dispatch<React.SetStateAction<responseRequest | undefined>>
+    likedMusics: string[];
+    likeOrDislikeMusic: (music: string) => void
 }
 
+
 export const SongList: React.FC<Props> = ({
-  songs,
-  setCurrentSong,
-  setSongs,
-  audioElmt,
-  isPlaying,
-  setIsPlaying,
-  setArtist,
-  isPlaylistSong,
-  isLikedPlaylist,
-  deleteMusicToThePlaylist,
-  setAlert,
-  likedMusics,
-  likeOrDislikeMusic,
-  setPlaylist,
-}) => {
-  const [allPlaylists, setAllPlaylists] = useState<Playlists>([]);
+                                              songs,
+                                              setCurrentSong,
+                                              setSongs,
+                                              audioElmt,
+                                              isPlaying,
+                                              setIsPlaying,
+                                              setArtist,
+                                              isPlaylistSong,
+                                              deleteMusicToThePlaylist,
+                                              setAlert,
+                                              likedMusics,
+                                              likeOrDislikeMusic
+                                          }) => {
 
-  const getAllPlaylistsOfTheUser = async () => {
-    try {
-      const res = await axios.get(
-        `${import.meta.env.VITE_APP_BACK_URL}/users/me/playlists`,
-        {
-          withCredentials: true,
-        }
-      );
-      setAllPlaylists(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+    const [allPlaylists, setAllPlaylists] = useState<Playlists>([])
 
-  const getALlMusicsCidOfAPlaylist = async (playlistId: number) => {
-    const musicsCid: string[] = [];
-    try {
-      const res = await axios.get(
-        `${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlistId}`,
-        {
-          withCredentials: true,
-        }
-      );
-      res.data.musics.forEach((music: any) => {
-        const musicUrl = music.src.split("/");
-        musicsCid.push(musicUrl[musicUrl.length - 1]);
-      });
-      return musicsCid;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const addSongToThePlaylist = async (playlistId: number, musicSrc: string) => {
-    let musicsCid: string[] | undefined = [];
-    try {
-      musicsCid = await getALlMusicsCidOfAPlaylist(playlistId);
-      const newMusicUrl = musicSrc.split("/");
-      musicsCid!.push(newMusicUrl[newMusicUrl.length - 1]);
-
-      const data = { musics: musicsCid };
-      const res = await axios.put(
-        `${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlistId}`,
-        data,
-        {
-          withCredentials: true,
-        }
-      );
-      if (Array.isArray(res.data)) {
-        displayAlert(res.data[0].msg, res.status);
-      } else {
-        displayAlert("Music added successfully to the playlist", res.status);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const displayAlert = (msg: string, status: number) => {
-    setAlert({ response: msg, status: status, visible: true });
-    setTimeout(() => {
-      setAlert({ response: "", status: 0, visible: false });
-    }, 3000);
-  };
-
-  useEffect(() => {
-    getAllPlaylistsOfTheUser();
-  }, []);
-
-  const foundCidMusic = (musicUrl: string) => {
-    const musicCIDArray = musicUrl.split("/");
-    return musicCIDArray[musicCIDArray.length - 1];
-  };
-
-  const isLikedMusics = (music: string) => {
-    const musicCID = foundCidMusic(music);
-    return likedMusics.includes(musicCID);
-  };
-
-  const likeOrDislikeMusicAccordingIfLikedPlaylist = (musicSrc: string) => {
-    likeOrDislikeMusic(musicSrc);
-    if (isLikedPlaylist) {
-      setPlaylist!({
-        ...songs,
-        musics: songs.musics.filter((music) => music.src !== musicSrc),
-      });
-    }
-  };
-
-  return (
-    <div className="container-list">
-      <ul className="list-song">
-        {songs?.musics.map((music, i) => (
-          <div key={i} className="flex flex-row hover:bg-teal-300">
-            <li
-              onClick={(e) => {
-                setCurrentSong(music);
-                setSongs(songs.musics);
-                setArtist(songs);
-                if (isPlaying) {
-                  setTimeout(() => {
-                    PlayPause(audioElmt, false, setIsPlaying);
-                  }, 1000);
-                } else {
-                  PlayPause(audioElmt, isPlaying, setIsPlaying);
+    const getAllPlaylistsOfTheUser = async () => {
+        try {
+            const res = await axios.get(`${import.meta.env.VITE_APP_BACK_URL}/users/me/playlists`, {
+                    withCredentials: true,
                 }
-              }}
-              className="song-li cursor-pointer"
-            >
-              <div className="avatar placeholder">
-                <div className="text-neutral-content rounded-full w-10">
-                  <span className="text-xl">0{i + 1}</span>
-                </div>
-              </div>
-              <div className="p-0 divider divider-horizontal" />
-              <div className="song-li-info">
-                <h4 style={{}}>{music.name}</h4>
-                <p>{songs.artist}</p>
-              </div>
-            </li>
-            <div className="song-li-action w-56 flex flex-row items-end pb-5">
-              <div>
-                {!isLikedPlaylist &&
-                  (isPlaylistSong ? (
-                    <div className="dropdown dropdown-bottom bg-transparent">
-                      <div tabIndex={0}>
-                        <BsThreeDotsVertical />
-                      </div>
-                      <div
-                        tabIndex={0}
-                        className="dropdown-content menu p-2 shadow bg-base-100 w-28 rounded-box hover:bg-white"
-                      >
-                        <button
-                          onClick={() => deleteMusicToThePlaylist(music.src!)}
-                        >
-                          Remove
-                        </button>
-                      </div>
-                    </div>
-                  ) : (
-                    <>
-                      <div className="dropdown dropdown-end">
-                        <div
-                          tabIndex={0}
-                          className="pt-2 rounded-lg cursor-pointer"
-                        >
-                          <IoAdd />
-                        </div>
-                        <ul
-                          tabIndex={0}
-                          className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 hover:bg-white items-center"
-                        >
-                          {allPlaylists.map((thePlaylist, i) => (
-                            <li key={i}>
-                              <button
-                                className="hover:bg-white self-center"
-                                onClick={() =>
-                                  addSongToThePlaylist(
-                                    thePlaylist.id,
-                                    music.src!
-                                  )
+            )
+            setAllPlaylists(res.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const getALlMusicsCidOfAPlaylist = async (playlistId: number) => {
+        const musicsCid: string[] = []
+        try {
+            const res = await axios.get(`${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlistId}`, {
+                    withCredentials: true,
+                }
+            )
+            res.data.musics.forEach((music: any) => {
+                const musicUrl = music.src.split('/')
+                musicsCid.push(musicUrl[musicUrl.length - 1])
+            })
+            return musicsCid
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const addSongToThePlaylist = async (playlistId: number, musicSrc: string) => {
+        let musicsCid: string[] | undefined = []
+        try {
+            musicsCid = await getALlMusicsCidOfAPlaylist(playlistId)
+            const newMusicUrl = musicSrc.split('/')
+            musicsCid!.push(newMusicUrl[newMusicUrl.length - 1])
+
+            const data = {musics: musicsCid}
+            const res = await axios.put(`${import.meta.env.VITE_APP_BACK_URL}/playlists/${playlistId}`, data, {
+                    withCredentials: true,
+                }
+            )
+            if (Array.isArray(res.data)) {
+                displayAlert(res.data[0].msg, res.status)
+            } else {
+                displayAlert('Music added successfully to the playlist', res.status)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const displayAlert = (msg: string, status: number) => {
+        setAlert({response: msg, status: status, visible: true})
+        setTimeout(() => {
+            setAlert({response: "", status: 0, visible: false})
+        }, 3000)
+    }
+
+    useEffect(() => {
+        getAllPlaylistsOfTheUser();
+    }, [])
+
+
+    const foundCidMusic = (musicUrl: string) => {
+        const musicCIDArray = musicUrl.split("/")
+        return musicCIDArray[musicCIDArray.length - 1]
+    }
+
+    const isLikedMusics = (music: string) => {
+        const musicCID = foundCidMusic(music)
+        return likedMusics.includes(musicCID);
+    }
+
+    return (
+        <div className='container-list'>
+            <ul className="list-song">
+                {songs?.musics.map((music, i) => (
+                    <div key={i} className="flex flex-row hover:bg-teal-300">
+                        <li
+                            onClick={e => {
+                                setCurrentSong(music)
+                                setSongs(songs.musics)
+                                setArtist(songs)
+                                if (isPlaying) {
+                                    setTimeout(() => {
+                                        PlayPause(audioElmt, false, setIsPlaying)
+                                    }, 1000)
+                                } else {
+                                    PlayPause(audioElmt, isPlaying, setIsPlaying)
                                 }
-                              >
-                                {thePlaylist.name}
-                              </button>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </>
-                  ))}
-              </div>
-              <button
-                className="pl-5"
-                onClick={() =>
-                  likeOrDislikeMusicAccordingIfLikedPlaylist(music.src!)
-                }
-              >
-                <Icon
-                  icon="like"
-                  color={isLikedMusics(music.src!) ? "red" : "black"}
-                  variant={isLikedMusics(music.src!) ? "Bold" : "Linear"}
-                />
-              </button>
-              <p className="pt-4 pl-5">2 : 30</p>
-            </div>
-          </div>
-        ))}
-      </ul>
-    </div>
-  );
-};
+                            }}
+                            className="song-li cursor-pointer"
+                        >
+                            <div className="avatar placeholder">
+                                <div className="text-neutral-content rounded-full w-10">
+                                    <span className="text-xl">0{i + 1}</span>
+                                </div>
+                            </div>
+                            <div className="p-0 divider divider-horizontal"/>
+                            <div className="song-li-info">
+                                <h4 style={{}}>{music.name}</h4>
+                                <p>{songs.artist}</p>
+                            </div>
+                        </li>
+                        <div className="song-li-action w-56 flex flex-row items-end pb-5">
+                            <div>
+                                {isPlaylistSong ?
+                                    <div className="dropdown dropdown-left bg-transparent">
+                                        <div tabIndex={0}>
+                                            <BsThreeDotsVertical/>
+                                        </div>
+                                        <div tabIndex={0}
+                                             className="dropdown-content menu p-2 shadow bg-base-100 w-28 rounded-box hover:bg-white">
+                                            <button onClick={() => deleteMusicToThePlaylist(music.src!)}>
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </div>
+                                    :
+                                    <>
+                                        <div className="dropdown dropdown-end">
+                                            <div tabIndex={0} className="pt-2 rounded-lg cursor-pointer">
+                                                <IoAdd/>
+                                            </div>
+                                            <ul tabIndex={0}
+                                                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 hover:bg-white items-center">
+                                                {allPlaylists.map((thePlaylist, i) => (
+                                                    <li key={i}>
+                                                        <button className="hover:bg-white self-center"
+                                                                onClick={() => addSongToThePlaylist(thePlaylist.id, music.src!)}
+                                                        >
+                                                            {thePlaylist.name}
+                                                        </button>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    </>
+                                }
+                            </div>
+                            <button className="pl-5" onClick={() => likeOrDislikeMusic(music.src!)}>
+                                <Icon icon="like" color={isLikedMusics(music.src!) ? "red" : "black"}
+                                      variant={isLikedMusics(music.src!) ? "Bold" : "Linear"}/>
+                            </button>
+                            <p className="pt-4 pl-5">2 : 30</p>
+                        </div>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    )
+}
