be31951ee781ac211d2cc5f8c8f9e6fc9f0cd585
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: api config/controller/router setup"
    This reverts commit 6c5b196e6fb32bc6f162ee8bd597969824137fc8.

--- backend/api/src/server.ts
+++ backend/api/src/server.ts
@@ -1,13 +1,28 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
-import env from '@config/env.config'
-import appRouter from '@routers/app.router'
+import config from './config'
+import DwavesToken from '@abi/DwavesToken.json'
 
-const { port, appName } = env
+const { port, appName } = config
 
 const app = express()
+const prisma = new PrismaClient()
 
-app.use(appRouter)
+app.get('/', (req: Request, res: Response) => {
+  const ok = '👌OK'
+  console.log(`[${appName}]: ${ok}`)
+  res.json(ok)
+})
+
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+app.get('/token', async (req: Request, res: Response) => {
+  res.json({ DwavesToken })
+})
 
 app.listen(port, () => {
   console.log(`[${appName}]: ⚡️Server is running at port ${port}`)
