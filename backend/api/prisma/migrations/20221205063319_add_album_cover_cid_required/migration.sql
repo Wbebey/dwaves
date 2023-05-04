54a7d787689d237081c631512580f288005e0eda
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: add album coverCID to album table"
    This reverts commit 0f3bf352063bb1e982b52cb63c95348b3ec1ed90.

--- backend/api/prisma/migrations/20221205063319_add_album_cover_cid_required/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `coverCID` on table `Album` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Album" ALTER COLUMN "coverCID" SET NOT NULL;
