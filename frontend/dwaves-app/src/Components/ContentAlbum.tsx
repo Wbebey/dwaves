0f77834e38ed823af826da4be1199773d20ceceb
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "refact: init file scss for list of albums and delete bcc useless"
    This reverts commit 4aeb5c59bbded4bc78bbb0f771a27de987ba007b.

--- frontend/dwaves-app/src/Components/ContentAlbum.tsx
+++ frontend/dwaves-app/src/Components/ContentAlbum.tsx
@@ -7,15 +7,15 @@ import { Link } from 'react-router-dom'
 
 export const ContentAlbum = () => {
     return (
-        <div  className="content-album">
-            <header className="head">
+        <div style={{ paddingRight: '5px', borderRadius: '4px', marginLeft: '16px', background: 'white', width: '100%', height: '100%' }} className="content">
+            <header style={{ padding: '7px', width: '100%', height: '10%', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap' }} className="head">
                 <Link to={'/'}>
                     <Back style={{ color: 'black', width: '32px', height: '32px' }} />
                 </Link>
                 <div className="divider divider-horizontal" />
-                <div className='title'>
-                    <img src={`${process.env.REACT_APP_URL}stamina1.jpg`} alt="" />
-                    <h3>
+                <div style={{ width: '90%', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap' }}>
+                    <img style={{ width: '55px', height: '55px' }} src={`${process.env.REACT_APP_URL}stamina1.jpg`} alt="" />
+                    <h3 style={{fontWeight: 'bold', fontSize: '2.5em', marginLeft: '5px' }}>
                         Stamina
                     </h3>
                 </div>
@@ -25,27 +25,27 @@ export const ContentAlbum = () => {
                     </div>
                 </div>
             </header>
-            <ul className='list-song'>
+            <ul style={{ width: '100%', height: '60%' }}>
                 {
                     datasongs.map((song, i) =>
-                        <li className='song-li'>
+                        <li style={{ marginTop: '10px', padding: '5px 0', width: '100%', height: '12%', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap' }}>
                             <div className="avatar placeholder">
                                 <div className="text-neutral-content rounded-full w-10">
                                     <span className="text-xl">0{i + 1}</span>
                                 </div>
                             </div>
                             <div className="p-0 divider divider-horizontal" />
-                            <div className='song-li-info'>
-                                <h4 style={{}}>
+                            <div style={{ width: '85%', height: '100%' }}>
+                                <h4 style={{fontWeight: 'bold', fontSize: '1.5em'}}>
                                     {song.Title}
                                 </h4>
                                 <p>
                                     {song.Artist}
                                 </p>
                             </div>
-                            <div className='song-li-action'>
-                                <AddCircle />
-                                <Heart />
+                            <div style={{ justifyContent: 'space-around', width: '10%', margin: '10px', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap' }}>
+                                <AddCircle style={{ width: '30px', height: '30px' }} />
+                                <Heart style={{ width: '30px', height: '30px' }} />
                                 <p>
                                     2 : 30
                                 </p>
