7d1da52c13d48edd0abf58452bc084e37dfc6f68
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: add custom logger"
    This reverts commit 2cbe401745667ba827c4ef211fcdb0145d94eeaa.

--- backend/api/src/server.ts
+++ backend/api/src/server.ts
@@ -2,7 +2,6 @@ import express from 'express'
 
 import env from '@config/env.config'
 import appRouter from '@routers/app.router'
-import logger from '@config/logger.config'
 
 const { port, appName } = env
 
@@ -11,5 +10,5 @@ const app = express()
 app.use(appRouter)
 
 app.listen(port, () => {
-  logger.log(`[${appName}]: ⚡️Server is running at port ${port}`)
+  console.log(`[${appName}]: ⚡️Server is running at port ${port}`)
 })
