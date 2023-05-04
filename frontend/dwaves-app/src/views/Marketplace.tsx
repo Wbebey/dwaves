4ff444cba69d5261f2cbd849283d4106ea79b06f
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: forms for Concert is done"
    This reverts commit 01e527c6d3d1771f708f0f5ff331046458c119ab.

--- frontend/dwaves-app/src/views/Marketplace.tsx
+++ frontend/dwaves-app/src/views/Marketplace.tsx
@@ -28,11 +28,11 @@ export const Marketplace: React.FC<Props> = ({
       {wallet === '' ? (
         <div className={'h-[97%] pt-[30px] pl-[20px]'}>
           <div className="w-full h-[90%] overflow-scroll flex flex-col items-center">
-            <p className="text-xl mt-5 mx-40 text-center">
-              Ready to explore the exciting offerings on Dwaves marketplace?
-              Simply log in to your MetaMask account to get started
+            <p className="text-xl mt-5">
+              To access the Dwaves marketplace, please log in to your MetaMask
+              account
             </p>
-            <p className="text-xl my-10">Click on the Fox !</p>
+            <p className="text-xl my-5">Click on the Fox !</p>
             <div className="cursor-pointer" onClick={requestConnectionMetamask}>
               <Kitsune />
             </div>
