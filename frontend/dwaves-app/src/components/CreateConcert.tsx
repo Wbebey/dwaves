722aa10abf42b9a33d7eafe1c7b861a66f72838b
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: form create concert is done"
    This reverts commit 530dc08feba4b853f4a0b2b1ba9ceeaf57049b42.

--- frontend/dwaves-app/src/components/CreateConcert.tsx
+++ frontend/dwaves-app/src/components/CreateConcert.tsx
@@ -24,7 +24,7 @@ const Input = ({ label, register, required, type = '' }: InputProps) => (
     <label className="text-lg">{label}</label>
     <input
       type={type}
-      className="bg-white h-9 w-72 mt-2 rounded-lg border-2 border-teal-300 text-center px-3"
+      className="bg-white h-10 w-72 mt-2 rounded-xl border-2 border-teal-300 text-center"
       placeholder={label}
       {...register(label, { required })}
     />
@@ -40,47 +40,23 @@ export const CreateConcert = () => {
   } = useForm<IFormValues>()
   const onSubmit: SubmitHandler<IFormValues> = (data) => console.log(data)
 
-  console.log('vibes' + watch('Ticket price in VIBES'))
   return (
     <div>
-      <h3 className="text-3xl text-center">
+      <h3 className="text-3xl text-center mb-5">
         Create a Decentralized Concert 🤯!
       </h3>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-row mx-10">
-          <div className="w-2/3 self-center flex items-center flex-col ml-5 mr-12">
-            <div>
-              <div className="absolute pl-5">
-                <p className="mt-5 text-4xl font-bol italic">
-                  {watch('Event name')}
-                </p>
-                <p className="mt-9 text-xl">{watch('Date')}</p>
-                <div className="mt-7 flex flex-row justify-between w-96">
-                  <p className="text-xl font-bold">{watch('Event place')}</p>
-                  <p className="text-xl">{watch('Genre')}</p>
-                  <p className="text-xl text-blue-600 font-bold">
-                    {watch('Ticket price in VIBES')}{' '}
-                    {watch('Ticket price in VIBES') !== null && 'VIBES'}
-                  </p>
-                </div>
-              </div>
-              <img src={ticketTemplate} alt="" />
-            </div>
-
-            <button
-              className="mt-10 self-center w-1/3 bg-teal-300 h-16 rounded-lg"
-              type="submit"
-            >
-              Mint {watch('Number of available tickets')} tickets NFTs
-            </button>
-          </div>
-          <div className="w-1/3">
+      <div className="flex flex-row mx-10">
+        <div className="w-2/3 bg-red-100 self-center ml-5 mr-12">
+          <img src={ticketTemplate} alt="" />
+        </div>
+        <div className="w-1/3">
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Input label={'Event name'} register={register} required={true} />
             <Input
               label={'Date'}
               register={register}
               required={true}
-              type={'datetime-local'}
+              type={'date'}
             />
             <Input
               label={'Number of available tickets'}
@@ -94,22 +70,22 @@ export const CreateConcert = () => {
               required={true}
               type={'number'}
             />
-            <div className="flex flex-col items-center mb-2">
-              <label className="text-lg">Event place</label>
-
-              <select
-                className="bg-white h-9 w-72 mt-2 rounded-lg border-2 border-teal-300 text-center px-3"
-                {...register('Event place')}
-              >
-                <option value=""></option>
-                <option value="MetaVerse">MetaVerse</option>
-                <option value="Teams">Teams</option>
-              </select>
-            </div>
-            <Input label={'Genre'} register={register} required={true} />
-          </div>
+            <Input
+              label={'Event place'}
+              register={register}
+              required={true}
+              type={'number'}
+            />
+            <Input
+              label={'Genre'}
+              register={register}
+              required={true}
+              type={'number'}
+            />
+            <button type="submit">HEY HEY</button>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
