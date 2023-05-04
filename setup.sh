81b8c8584c7cdad184a80cfb1f5ec5daf3e26fb9
Alex <alexandre@yakow.io>
2023-05-04 05:03:04 +0200

    Revert "feat: add pre-commit"
    This reverts commit 5033606af23f5fe88d3655dca5647322e362464c.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This script is used to install pre-commit hooks
+# This script is used to setup requirement for pre-commit hooks for the project.
+# It is assumed that the script is run from the root of the project.
 
 set -euxo pipefail
 
@@ -8,12 +9,14 @@ install_pre_commit() {
     if ! [ -x "$(command -v pre-commit)" ]
     then
         echo "Installing pre-commit"
-        pip install ggshield || pip3 install ggshield
-        pip install python-dotenv || pip3 install python-dotenv
+        # pip install ggshield || pip3 install ggshield
+        # pip install python-dotenv || pip3 install python-dotenv
         pip install pre-commit || pip3 install pre-commit
     else
         echo "pre-commit is already installed"
     fi
+    pip install ggshield || pip3 install ggshield
+    pip install python-dotenv || pip3 install python-dotenv
     pre-commit install 
 }
 
